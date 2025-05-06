61dbbc64afe5eb125798a51beb3738d696231527
Bekzod Akbarov <stx.webdev@gmail.com>
2025-05-06 16:56:38 +0500

    ai chat with memory

--- src/memory.ts
+++ src/memory.ts
@@ -7,26 +7,29 @@ export type MessageWithMetadata = AIMessage & {
   createdAt: string
 }
 
-export const addMetadata = (message: AIMessage): MessageWithMetadata => ({
-  ...message,
-  id: uuidv4(),
-  createdAt: new Date().toISOString(),
-})
-
-export const removeMetadata = (message: MessageWithMetadata): AIMessage => {
-  const { id, createdAt, ...messageWithoutMetadata } = message
-  return messageWithoutMetadata
-}
-
 type Data = {
   messages: MessageWithMetadata[]
 }
 
-const defaultData: Data = { messages: [] }
+const defaultData: Data = {
+  messages: [],
+}
+
+export const addMetadata = (message: AIMessage) => {
+  return {
+    ...message,
+    id: uuidv4(),
+    createdAt: new Date().toISOString(),
+  }
+}
+
+export const removeMetadata = (message: MessageWithMetadata) => {
+  const { id, createdAt, ...rest } = message
+  return rest
+}
 
 export const getDb = async () => {
   const db = await JSONFilePreset<Data>('db.json', defaultData)
-
   return db
 }
 
@@ -39,19 +42,4 @@ export const addMessages = async (messages: AIMessage[]) => {
 export const getMessages = async () => {
   const db = await getDb()
   return db.data.messages.map(removeMetadata)
-}
-
-export const saveToolResponse = async (
-  toolCallId: string,
-  toolResponse: string
-) => {
-  return await addMessages([
-    { role: 'tool', content: toolResponse, tool_call_id: toolCallId },
-  ])
-}
-
-export const clearMessages = async (keepLast?: number) => {
-  const db = await getDb()
-  db.data.messages = db.data.messages.slice(-(keepLast ?? 0))
-  await db.write()
 }
